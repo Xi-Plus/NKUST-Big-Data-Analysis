3ea8f532afad79abf92e902a95d32ce0d7d66da7
Xi-Plus <huangxuanyuxiplus@gmail.com>
2019-03-19 14:15:00 +0800

    hw1: Add some urls

--- hw1-regular-expression/main.py
+++ hw1-regular-expression/main.py
@@ -26,6 +26,11 @@ else:
     urls = [
         'http://www.csie.kuas.edu.tw/teacher.php',
         'http://www.csie.ncku.edu.tw/ncku_csie/depmember/teacher',
+        'http://www.shu.edu.tw/SHU-Contact.aspx',
+        'https://www.taftw.org.tw/wSite/ct?xItem=61&ctNode=283',
+        'https://www.cga.gov.tw/GipOpen/wSite/lp?ctNode=9331&mp=9991/&idPath=9238_9331',
+        'http://1www.tnua.edu.tw/~TNUA_THEATRE/members/teacher.php',
+        'http://www.fju.edu.tw/article.jsp?articleID=13',
     ]
     for url in urls:
         fetch_and_print(url)
