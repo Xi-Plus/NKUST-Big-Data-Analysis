22ce51ada6a8bdfb4bcf3b2218d3793668806e47
Xi-Plus <huangxuanyuxiplus@gmail.com>
2019-05-11 18:16:54 +0800

    [WIP] fp: Update L1Cmp

--- hw5-FP-growth-algorithm/fpgrowth.cpp
+++ hw5-FP-growth-algorithm/fpgrowth.cpp
@@ -152,6 +152,9 @@ class FPGrowth {
 	}
 
 	static bool L1Cmp(const pair<int, int> &a, const pair<int, int> &b) {
+		if (a.second == b.second) {
+			return a.first < b.first;
+		}
 		return a.second > b.second;
 	}
 
