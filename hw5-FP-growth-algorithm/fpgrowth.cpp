686343bfffea25dd57cba7c10c0501995baf9277
Xi-Plus <huangxuanyuxiplus@gmail.com>
2019-05-13 08:07:10 +0800

    fp: Remove sort by item in L1Cmp

--- hw5-FP-growth-algorithm/fpgrowth.cpp
+++ hw5-FP-growth-algorithm/fpgrowth.cpp
@@ -128,9 +128,6 @@ class FPGrowth {
 	}
 
 	static bool L1Cmp(const pair<int, int> &a, const pair<int, int> &b) {
-		if (a.second == b.second) {
-			return a.first < b.first;
-		}
 		return a.second > b.second;
 	}
 
